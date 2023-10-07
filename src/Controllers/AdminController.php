727abc735b300984a9795dc19c53fd66ee544153
Fabio Doppio <mail@fabiodoppio.de>
2023-10-07 12:53:22 +0200

    update userAction

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -15,6 +15,7 @@ namespace MVC\Controllers;
 
 use MVC\Ajax      as Ajax;
 use MVC\Auth      as Auth;
+use MVC\Database  as Database;
 use MVC\Exception as Exception;
 use MVC\Fairplay  as Fairplay;
 use MVC\Models    as Model;
@@ -42,20 +43,43 @@ class AdminController extends AccountController {
     public function userAction() {
         switch(Request::get("requestParts")[2]??"") {
             case "edit":
-                Auth::update_account(
-                    Fairplay::integer(Request::get("id")),
-                    Fairplay::username(Request::get("username")), 
-                    Fairplay::email(Request::get("email")),
-                    Fairplay::password(Request::get("pw1"), Request::get("pw2")));
+                $account = new Model\Account(Fairplay::integer(Request::get("id")));
+
+                if (Request::isset("username")) 
+                    if ($account->get("username") != Fairplay::username(Request::get("username"))) {
+                        if (!empty(Database::select("app_accounts", "username LIKE '".Request::get("username")."'")[0]))
+                            throw new Exception(_("Your entered username is already taken."));
+
+                        $account->set("username", Request::get("username"));
+                    }
+
+                if (Request::isset("email")) 
+                    if ($account->get("email") != Fairplay::email(Request::get("email"))) {
+                        if (!empty(Database::select("app_accounts", "email LIKE '".Request::get("email")."'")[0]))
+                            throw new Exception(_("Your entered email address is already taken."));
+    
+                        $account->set("email", strtolower(Request::get("email")));
+                    }
+
+                if (Request::isset("role"))
+                    if ($account->get("role") != Fairplay::integer(Request::get("role"))) {
+                        if ($account->get("id") == $this->account->get("id"))
+                            throw new Exception(_("You can not change your own role."));
+
+                        $account->set("role", Request::get("role"));
+                    }
+
+                if (Request::isset("pw1") && Request::isset("pw2"))
+                    if (Fairplay::password(Request::get("pw1"), Request::get("pw2")) != "")
+                        $account->set("password", password_hash(Request::get("pw1"), PASSWORD_DEFAULT));
 
                 Ajax::add('.response', '<div class="success">'._("Changes saved successfully.").'</div>');
                 break;
             case "delete":
-                if ($this->account->get("id") == Request::get("id"))
+                if ($this->account->get("id") == Fairplay::integer(Request::get("id")))
                     throw new Exception(_("You can not delete yourself."));
 
-                Auth::delete_account(Fairplay::integer(Request::get("id")));
-
+                Database::delete("app_accounts", "id = '".Request::get("id")."'");
                 Ajax::add('.response', '<div class="success">'._("User deleted successfully.").'</div>');
         }
     }
