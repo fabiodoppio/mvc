042693f681dc8e52920b4b74946d67d5ecf10364
Fabio Doppio <mail@fabiodoppio.de>
2023-11-02 14:58:58 +0100

    fixes for major update 1.5

--- src/App.php
+++ src/App.php
@@ -66,7 +66,7 @@ class App {
     protected static $MAIL_USERNAME     = "";
     protected static $MAIL_PASSWORD     = "";
 
-    protected static $META_EDITABLE     = "[\"email\"]";
+    protected static $META_EDITABLE     = "[\"email\",\"password\"]";
 
     /**
      * Initialize the application based on the provided configuration.
