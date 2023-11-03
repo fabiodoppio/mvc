2730d1f72001ca1f71ac74019aa67123d59de068
Fabio Doppio <mail@fabiodoppio.de>
2023-11-03 01:24:05 +0100

    fixes for major update 1.5

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -131,7 +131,7 @@ class AdminController extends AccountController {
     
                 if (Request::isset("title") && Request::get("title") != $page->get("title")) {
                     $page->set("title", Fairplay::string(Request::get("title")));
-                    Ajax::add('.list-item[data-id="'.$page->get("id").'"] .title', Request::get("title"));
+                    Ajax::add('.list-item[data-id="'.Request::get("id").'"] .title', Request::get("title"));
                 }
                      
                 if (Request::isset("slug") && Request::get("slug") != $page->get("slug")) {
@@ -139,7 +139,7 @@ class AdminController extends AccountController {
                         throw new Exception(_("Your entered slug is already used."));
 
                     $page->set("slug", Request::get("slug"));
-                    Ajax::add('.list-item[data-id="'.$page->get("id").'"] .slug', "slug:".Request::get("slug"));
+                    Ajax::add('.list-item[data-id="'.Request::get("id").'"] .slug', "slug:".Request::get("slug"));
                 }
 
                 if (Request::isset("description") && Request::get("description") != $page->get("description"))
