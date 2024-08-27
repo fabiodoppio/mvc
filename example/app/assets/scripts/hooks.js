b1eb51188921f2d26d6800e4b4c4f8f30a38d948
Fabio Doppio <mail@fabiodoppio.de>
2024-08-27 12:30:02 +0200

    update functions

--- example/app/assets/scripts/hooks.js
+++ example/app/assets/scripts/hooks.js
@@ -1,19 +1,19 @@
-function ajax_send_before(data) {
+function ajax_send_before(data, context) {
     return true;
 }
 
-function ajax_send_done(res) {
+function ajax_send_done(res, context) {
     return true;
 }
 
-function ajax_send_fail(res) {
+function ajax_send_fail(res, context) {
     return true;
 }
 
-function ajax_send_always() {
+function ajax_send_always(context) {
     return true;
 }
 
-function ajax_send_after() {
+function ajax_send_after(context) {
     return true;
 }
