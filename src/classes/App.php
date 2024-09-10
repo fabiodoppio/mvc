75814be8e95e77f3b7e0f00acbc92d477e725d7c
Fabio Doppio <mail@fabiodoppio.de>
2024-09-10 12:54:07 +0200

    add optional parameter  to generate_token();

--- src/classes/App.php
+++ src/classes/App.php
@@ -287,14 +287,14 @@ class App {
      *  @return string  The generated token.
      * 
      */
-    public static function generate_token() {
+    public static function generate_token(?int $length = 32) {
         $token = "";
         $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
         $codeAlphabet.= "0123456789";
         $max = strlen($codeAlphabet);
     
-        for ($i=0; $i < 32; $i++)
+        for ($i=0; $i < $length; $i++)
             $token .= $codeAlphabet[random_int(0, $max-1)];
     
         return $token;
