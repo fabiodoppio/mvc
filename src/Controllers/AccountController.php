cd7b280bb257c3c1d82c4056d5044e219a96d577
Fabio Doppio <hallo@fabiodoppio.de>
2025-11-15 00:34:33 +0100

    add doc

--- src/Controllers/AccountController.php
+++ src/Controllers/AccountController.php
@@ -476,6 +476,7 @@ class AccountController extends Controller {
      *
      *  Handles requests from any contact form.
      *
+     *  @since  3.05    Added ratelimit for contact forms to prevent spam.
      *  @since  2.0
      *  @param  string  $request    The requested action
      *
